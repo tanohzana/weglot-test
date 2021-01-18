f42e9bea4bb1c7fa4131fa2bbe32d2b730792ac0
Florian Adonis <tanohzana@gmail.com>
2021-01-18 23:09:06 +0000

    fix test and fetch output file

--- src/utils/tests/findHour.test.js
+++ src/utils/tests/findHour.test.js
@@ -1,8 +1,9 @@
+import fs from 'fs';
 import findHour from '../findHour';
 
 describe ('findHour function', () => {
   it ('returns the right interval for the meeting', () => {
-    const EXPECTED_MEETING = '3 13:18-14:17';
+    const EXPECTED_MEETING = fs.readFileSync(`${__dirname}/../../../data/output5.txt`, 'utf8');
     const meeting = findHour('input5.txt');
 
     expect(meeting).toBe(EXPECTED_MEETING);
